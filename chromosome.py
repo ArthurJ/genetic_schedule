a3698f870e16f26dd67793accf13447898add05d
Arthur Julião <azrael.zila@gmail.com>
2019-01-25 12:10:31 -0200

    First commit, just creating the main files

---
+++ chromosome.py
@@ -0,0 +1,35 @@
+__author__ = '@arthurj'
+
+class Chromosome:
+    '''
+
+    '''
+    def __init__(self, *args, **kwargs):
+        pass
+    
+    def evaluate_fitness(self):
+        pass
+    
+    def crash_detection(self):
+        pass
+    
+    def __le__(self, value):
+        return False
+    
+    def __lt__(self, value):
+        return False
+    
+    def __ge__(self, value):
+        return False
+
+    def __gt__(self, value):
+        return False
+    
+    def __eq__(self, value):
+        return False
+    
+    def __hash__(self):
+        return 0
+    
+    def __str__(self):
+        return ''
---
+++ population.py
@@ -0,0 +1,31 @@
+__author__ = '@arthurj'
+
+def combine(genes_a, genes_b):
+    pass
+
+def sex():
+    pass
+
+def assex():
+    pass
+
+class Population:
+    
+    def __init__(self, *args, **kwargs):
+        pass
+    
+    def selection(self):
+        pass
+    
+    def mesure_fitness(self):
+        pass
+    
+    def next_generation(self):
+        pass
+    
+    
+    def __str__(self):
+        return ''
+    
+    def __repr__(self):
+        return ''
